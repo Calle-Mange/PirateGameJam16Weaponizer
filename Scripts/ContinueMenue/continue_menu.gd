a319b0755d7d38126327194bc87fc890c957a383
Felix Persson <felix.me.persson@gmail.com>
2025-01-28 21:20:12 +0100

    continue screen

---
+++ Scripts/ContinueMenue/continue_menu.gd
@@ -0,0 +1,63 @@
+extends CanvasLayer
+@onready
+var continueButton = $MarginContainer/PanelContainer/MarginContainer/Node2D/ContinueButton
+@onready
+var exitButton = $MarginContainer/PanelContainer/MarginContainer/Node2D/ExitButton
+@onready var level_manager = $"/root/LevelManager"
+
+var currentSelector = 0
+var selectColor = Color(1, 0.953, 0);
+var NoSelectColor = Color(0.875, 0.875, 0.875);
+
+func _ready() -> void:
+	set_current_selector(currentSelector)
+
+func _on_continue_button_pressed() -> void:
+	print("continue pressed")
+	level_manager.ChangeSceneToFile("res://Scenes/TileMapLevels/map0.tscn")
+
+
+func _on_exit_button_pressed() -> void:
+	print("exit pressed")
+	level_manager.ChangeSceneToFile("res://Scenes/Menu/main_menu.tscn")
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(_delta: float) -> void:	
+	if (Input.is_action_just_pressed("ui_down") && currentSelector != 1):
+		print("down")
+		currentSelector += 1
+		set_current_selector(currentSelector)
+	if (Input.is_action_just_pressed("ui_up") && currentSelector != 0):
+		print("up")
+		currentSelector -= 1
+		set_current_selector(currentSelector)
+	if (Input.is_action_just_pressed("ui_accept")):
+		print("ok")
+		select_current_option(currentSelector)
+
+func set_current_selector(_currentSelector) -> void:
+	#continueButton.add_theme_color_override("font_color", NoSelectColor)
+	#exitButton.add_theme_color_override("font_color", NoSelectColor)
+	if _currentSelector == 0:
+		continueButton.add_theme_color_override("font_color", selectColor)
+		exitButton.add_theme_color_override("font_color", NoSelectColor)
+	elif _currentSelector == 1:
+		exitButton.add_theme_color_override("font_color", selectColor)
+		continueButton.add_theme_color_override("font_color", NoSelectColor)
+		
+func select_current_option(_currentSelector) -> void:
+	if _currentSelector == 0:
+		level_manager.ChangeSceneToFile("res://Scenes/Menu/story_scene.tscn")
+		# get_parent().add_child(map0Scene.instantiate())
+		queue_free()
+		
+	elif _currentSelector == 1:
+		get_tree().quit()
+
+func _on_continue_button_mouse_entered() -> void:
+	continueButton.add_theme_color_override("font_color", selectColor)
+	exitButton.add_theme_color_override("font_color", NoSelectColor)
+
+func _on_exit_button_mouse_entered() -> void:
+	continueButton.add_theme_color_override("font_color", NoSelectColor)
+	exitButton.add_theme_color_override("font_color", selectColor)
